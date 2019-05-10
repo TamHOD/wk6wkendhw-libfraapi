08faf0138f1605fb8722d319efa6f8560e3019c1
Tam O'Donnell <thomashodonnell@gmail.com>
2019-05-10 15:10:11 +0100

    add js beginning

--- js/app.js
+++ js/app.js
@@ -1,8 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // const newItemform = document.querySelector('#new-item-form');
-    // newItemform.addEventListener('submit', handleNewItemFormSubmit);
-
-    // const deleteAllButton = document.querySelector('#delete-all');
-    // deleteAllButton.addEventListener('click', handleDeleteAllClick);
+    const submitForm = document.querySelector('#form');
+    submitForm.addEventListener('submit', handleSubmitForm);
 });
 
+const handleSubmitForm = function (event) {
+    event.preventDefault();
+
+    const resultListItem = document.querySelector('#stuff-to-learn-about')
+
+
+    const tr = document.createElement('tr')
+    resultListItem.appendChild(tr)
+
+    const title = document.createElement('td')
+    tr.appendChild(title)
+    title.textContent = `${event.target.title.value}`
+
+    const website = document.createElement('td')
+    tr.appendChild(website)
+    title.textContent = `${event.target.website.value}`
+
+    const type = document.createElement('td')
+    tr.appendChild(type)
+    title.textContent = `${event.target.type.value}`
+
+    // const lang = document.createElement('td')
+    // tr.appendChild(lang)
+    // title.textContent = `${event.target.lang.value}`
+
+
+}
