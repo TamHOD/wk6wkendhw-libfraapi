6435b42fcb38a9a652caf0f80c9b1a0b47681182
Tam O'Donnell <thomashodonnell@gmail.com>
2019-05-10 14:52:12 +0100

    setup html

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // const newItemform = document.querySelector('#new-item-form');
+    // newItemform.addEventListener('submit', handleNewItemFormSubmit);
+
+    // const deleteAllButton = document.querySelector('#delete-all');
+    // deleteAllButton.addEventListener('click', handleDeleteAllClick);
+});
+
