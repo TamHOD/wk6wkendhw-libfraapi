8a6a73187a870271b43a3a9f1da4a0a6fb047b4b
Tam O'Donnell <thomashodonnell@gmail.com>
2019-05-13 07:41:33 +0100

    refactor, add css etc.

--- js/app.js
+++ js/app.js
@@ -6,35 +6,35 @@ document.addEventListener('DOMContentLoaded', () => {
 const handleSubmitForm = function (event) {
     event.preventDefault();
 
-    const resultListItem = document.querySelector('#stuff-to-learn-about')
+    const resultListItem = document.querySelector('#stuff-to-learn-about');
 
 
-    const tr = document.createElement('tr')
-    resultListItem.appendChild(tr)
+    const tr = document.createElement('tr');
+    resultListItem.appendChild(tr);
 
-    const title = document.createElement('td')
-    tr.appendChild(title)
-    title.textContent = `${event.target.title.value}`
+    const title = document.createElement('td');
+    tr.appendChild(title);
+    title.textContent = `${event.target.title.value}`;
 
-    const website = document.createElement('td')
-    tr.appendChild(website)
-    website.textContent = `${event.target.website.value}`
+    const website = document.createElement('td');
+    tr.appendChild(website);
+    website.textContent = `${event.target.website.value}`;
 
-    const type = document.createElement('td')
-    tr.appendChild(type)
-    type.textContent = `${event.target.type.value}`
+    const type = document.createElement('td');
+    tr.appendChild(type);
+    type.textContent = `${event.target.type.value}`;
 
-    const lang = document.createElement('td')
-    tr.appendChild(lang)
-    lang.textContent = `${event.target.lang.value}`
+    const lang = document.createElement('td');
+    tr.appendChild(lang);
+    lang.textContent = `${event.target.lang.value}`;
 
-    const learned = document.createElement('td')
-    tr.appendChild(learned)
-    learned.textContent = `${event.target.learned.value}`
-    
-    const description = document.createElement('td')
-    tr.appendChild(description)
-    description.textContent = `${event.target.description.value}`
+    const learned = document.createElement('td');
+    tr.appendChild(learned);
+    learned.textContent = `${event.target.learned.value}`;
+
+    const description = document.createElement('td');
+    tr.appendChild(description);
+    description.textContent = `${event.target.description.value}`;
 
     
 }
